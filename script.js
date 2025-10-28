51cbeb7629d2d8531180ac19f93b923ca59761df
iliyazbegaliuulu-hub <iliyazbegaliuulu@gmail.com>
2025-10-28 15:02:31 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let name =prompt (85)
+if (name<=50) {alert('өтпөй калдың')}
+else if (name<=70) { alert('канаттандыраарлык')}
+else if (name<=89){ alert('жакшы')}
+else if (name=>90) {alert('азамат')}
+else ('азамат')
+
